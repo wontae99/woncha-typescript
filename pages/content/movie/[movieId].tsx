7893892a9f87b47e1928794889a5cc8e22cdd09d
wontae <wjs152828@gmail.com>
2023-08-24 21:15:49 +0900

    타입 재설정

--- pages/content/movie/[movieId].tsx
+++ pages/content/movie/[movieId].tsx
@@ -1,38 +1,35 @@
 import { Fragment } from "react";
 import Head from "next/head";
+// types
+import type {
+  GetStaticProps,
+  GetStaticPaths,
+} from "next";
+import { ContentData } from "@/lib/types";
+// libs for fetching data
 import {
   getAllContentIds,
   getDataWithId,
   getVideoWithId,
 } from "@/lib/movie-data";
-
+// components
 import Profile from "@/components/profile-page/profile";
 
-export default function MovieDetailPage({ data, contentId }) {
-  return (
-    <Fragment>
-      <Head>
-        <title>Movie - {data.title}</title>
-        <meta name="description" content={data.overview} />
-      </Head>
-      <Profile data={data} type={"movie"} contentId={contentId} />
-    </Fragment>
-  );
-}
-
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const contentIdList = await getAllContentIds("movie");
-  const paths = contentIdList.map((id) => ({
+  const paths = contentIdList.map((id: string) => ({
     params: { movieId: id },
   }));
 
   return {
-    paths: paths,
-    fallback: false,
+    paths,
+    fallback: "blocking",
   };
-}
+};
 
-export async function getStaticProps(context) {
+export const getStaticProps: GetStaticProps = async (context: {
+  params: { movieId: string };
+}) => {
   const contentId = context.params.movieId;
   const data = await getDataWithId("movie", contentId);
   const contentVideo = await getVideoWithId("movie", contentId);
@@ -43,9 +40,27 @@ export async function getStaticProps(context) {
 
   return {
     props: {
-      data: data,
+      data,
       contentId,
     },
     revalidate: 1,
   };
+};
+
+export default function MovieDetailPage({
+  data,
+  contentId,
+}: {
+  data: ContentData;
+  contentId: string;
+}) {
+  return (
+    <Fragment>
+      <Head>
+        <title>Movie - {data.title}</title>
+        <meta name="description" content={data.overview} />
+      </Head>
+      <Profile data={data} type={"movie"} contentId={contentId} />
+    </Fragment>
+  );
 }
--- pages/content/tv/[tvShowId].tsx
+++ pages/content/tv/[tvShowId].tsx
@@ -1,38 +1,35 @@
 import { Fragment } from "react";
 import Head from "next/head";
-import Profile from "@/components/profile-page/profile";
-
+// types
+import type {
+  GetStaticProps,
+  GetStaticPaths,
+} from "next";
+// libs for data-fetching
 import {
   getAllContentIds,
   getDataWithId,
   getVideoWithId,
 } from "@/lib/movie-data";
+//components
+import Profile from "@/components/profile-page/profile";
+import { ContentData } from "@/lib/types";
 
-export default function TvShowDetailPage({ data, contentId }) {
-  return (
-    <Fragment>
-      <Head>
-        <title>TV show - {data.name}</title>
-        <meta name="description" content={data.overview} />
-      </Head>
-      <Profile data={data} type={"tv"} contentId={contentId} />
-    </Fragment>
-  );
-}
-
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const contentIdList = await getAllContentIds("tv");
   const paths = contentIdList.map((id) => ({
     params: { tvShowId: id },
   }));
 
   return {
-    paths: paths,
-    fallback: false,
+    paths,
+    fallback: "blocking",
   };
-}
+};
 
-export async function getStaticProps(context) {
+export const getStaticProps: GetStaticProps = async (context: {
+  params: { tvShowId: string };
+}) => {
   const contentId = context.params.tvShowId;
   const data = await getDataWithId("tv", contentId);
   const contentVideo = await getVideoWithId("tv", contentId);
@@ -43,9 +40,27 @@ export async function getStaticProps(context) {
 
   return {
     props: {
-      data: data,
+      data,
       contentId,
     },
     revalidate: 1,
   };
+};
+
+export default function TvShowDetailPage({
+  data,
+  contentId,
+}: {
+  data: ContentData;
+  contentId: string;
+}) {
+  return (
+    <Fragment>
+      <Head>
+        <title>TV show - {data.name}</title>
+        <meta name="description" content={data.overview} />
+      </Head>
+      <Profile data={data} type={"tv"} contentId={contentId} />
+    </Fragment>
+  );
 }
